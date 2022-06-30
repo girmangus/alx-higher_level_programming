fcbec852a066f79a0b4ae252dd9fca2c73a85442
Girmangus Hailu <girmangus@milkta.com>
2022-06-30 23:31:18 +0300

    0x02-python-import_modules advanced

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,5 +4,4 @@ if __name__ == "__main__":
     import hidden_4
     for n in dir(hidden_4):
         if n[:2] != "__":
-            #print(n)
-            print("{}".format(n))
+            print(n)
