611d2418bb0c0286bb32ae03b7f932191ace6820
Girmangus Hailu <girmangus@milkta.com>
2022-07-19 14:19:49 +0300

    0x06. Python - Classes and Objects advance

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,21 +1,27 @@
 #!/usr/bin/python3
-"""Class docstring"""
+"""MagicClass module.
+This module contains the MagicClass class used for the bytecode exercise.
+"""
 import math
 
+class MagicClass():
+    """Defines a MagicClass object."""
 
-class MagicClass:
     def __init__(self, radius=0):
-        """Init docstring
+        """Sets the necessary attributes for the MagicClass object.
+
+        Args:
+            radius (int, float): the radius of the circle
         """
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
-                raise TypeError('radius must be a number')
+            raise TypeError("radius must be a number")
         self.__radius = radius
 
     def area(self):
-        """Area docstring"""
+        """Returns the current circle area."""
         return self.__radius ** 2 * math.pi
 
     def circumference(self):
-        """Circumference docstring"""
+        """Returns the current circle circumference."""
         return 2 * math.pi * self.__radius
