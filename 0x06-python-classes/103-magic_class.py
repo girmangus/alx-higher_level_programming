7598182bce334be109aa8e7416ad026f4c416f57
Girmangus Hailu <girmangus@milkta.com>
2022-07-19 14:17:54 +0300

    0x06. Python - Classes and Objects advance

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
+"""Class docstring"""
 import math
 
 
 class MagicClass:
-    """ class that create circle area"""
-
     def __init__(self, radius=0):
-        """ initializing """
+        """Init docstring
+        """
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
-            raise TypeError('radius must be a number')
+                raise TypeError('radius must be a number')
         self.__radius = radius
 
     def area(self):
-        """ function defining area """
-        return (self.__radius ** 2 * math.pi)
+        """Area docstring"""
+        return self.__radius ** 2 * math.pi
 
     def circumference(self):
-        """ function defining circuference """
-        return ((2 * math.pi) * self.__radius)
+        """Circumference docstring"""
+        return 2 * math.pi * self.__radius
