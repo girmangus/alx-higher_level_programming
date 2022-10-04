5ffc430b4bcbf363c6aaf28208ae76d1cea8f3ec
Girmangus Hailu <girmangus@milkta.com>
2022-10-05 02:01:51 +0300

    0x13. JavaScript - Objects, Scopes and Closures advance

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -2,15 +2,13 @@
 
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
+    if (w > 0 && h > 0) { 
+      [this.width, this.height] = [w, h]; 
     }
   }
+
   print () {
-    let i = 0;
-    for (i = 0; i < this.height; i++) {
+    for (let i = 0; i < this.height; i++) 
       console.log('X'.repeat(this.width));
-    }
   }
 };
