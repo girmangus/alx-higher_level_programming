ef98c9d9f104f075a9da573219d5b6194cbc6dd5
Girmangus Hailu <girmangus@milkta.com>
2022-11-15 14:25:36 +0300

    0x14-javascript-web_scraping

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,25 +1,21 @@
 #!/usr/bin/node
 const request = require('request');
-const arg = process.argv;
-const URL = arg[2];
-const object = {};
-request.get(URL, (err, res, body) => {
-  if (err) {
-    console.log(err);
-  } else {
-    let user = 'default';
-    const data = JSON.parse(body);
-    for (let j = 0; j < data.length; j++) {
-      if (data[j].completed === true) {
-        if (!(data[j].userId in object)) {
-          user = data[j].userId;
-          object[user] = 1;
-        } else {
-          user = data[j].userId;
-          object[user] += 1;
-        }
-      }
+
+if (process.argv.length > 2) {
+  request(process.argv[2], (err, res, body) => {
+    const aggregate = {};
+
+    if (err) {
+      console.log(err);
     }
-    console.log(object);
-  }
-});
+    JSON.parse(body).forEach(element => {
+      if (element.completed) {
+        if (!aggregate[element.userId]) {
+          aggregate[element.userId] = 0;
+        }
+        aggregate[element.userId]++;
+      }
+    });
+    console.log(aggregate);
+  });
+}
